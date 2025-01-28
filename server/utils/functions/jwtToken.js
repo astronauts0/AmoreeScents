7834bd2e610d54513182ecf9e0064dac47b5bfd3
Tayyab <tayyabafzal174@gmail>
2025-01-29 00:37:02 +0500

    test cookie in jwtToken

--- server/utils/functions/jwtToken.js
+++ server/utils/functions/jwtToken.js
@@ -1,18 +1,13 @@
 const sendToken = async (user, res) => {
   const token = await user.generateToken();
-
-  const isProduction = process.env.NODE_ENV === "PRODUCTION";
-  
-  
-  const cookieExpireDays = Number(process.env.COOKIE_EXPIRE); 
+  const isProduction = process.env.NODE_ENV === "production";
+  const cookieExpireDays = Number(process.env.COOKIE_EXPIRE);
 
   const options = {
-    expires: new Date(
-      Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: isProduction ? "strict" : "lax",
-    secure: isProduction ? true : false,
+    sameSite: isProduction ? "none" : "lax",
+    secure: isProduction,
   };
 
   res
@@ -20,5 +15,3 @@ const sendToken = async (user, res) => {
     .cookie("token", token, options)
     .json({ success: true, user, token });
 };
-
-module.exports = sendToken;
