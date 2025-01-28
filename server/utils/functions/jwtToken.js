ce0d8b10692a80dfd8a509930b1fc3d723f19372
Tayyab <tayyabafzal174@gmail>
2025-01-28 23:36:13 +0500

    test cookie in jwtToken

--- server/utils/functions/jwtToken.js
+++ server/utils/functions/jwtToken.js
@@ -2,16 +2,15 @@ const sendToken = async (user, res) => {
   const token = await user.generateToken();
 
   const isProduction = process.env.NODE_ENV === "PRODUCTION";
-  
-  const cookieExpireDays = Number(process.env.COOKIE_EXPIRE); 
+
+  const cookieExpireDays = Number(process.env.COOKIE_EXPIRE);
 
   const options = {
-    expires: new Date(
-      Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: isProduction ? "Strict" : "Lax",
-    secure: isProduction ? true : false,
+    sameSite: "None", // Required for cross-domain cookies
+    secure: isProduction ? true : false, // Ensure cookie is sent over HTTPS in production
+    domain: ".amoreescents.shop", // Allow cookie for the entire domain (including subdomains)
   };
 
   res
